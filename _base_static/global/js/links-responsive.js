14c8dd1887dc0235e3cec1a8c86baddd6f368cce
enzortorres <enzortorres2005@gmail.com>
2025-05-08 21:39:08 -0300

    header responsivo concluido
    menu hamburguer com background escuro para foco no menu

---
+++ _base_static/global/js/links-responsive.js
@@ -0,0 +1,28 @@
+//! Menu Responsivo
+const link_menu = document.querySelector('.toggle');
+const overlay = document.querySelector('.overlay')
+const close = document.querySelector(".close-menu");
+const menu = document.querySelector(".links");
+var menuIsOpen = false;
+
+link_menu.addEventListener('click', () => {
+    overlay.style.display = 'block';
+    menu.style.right = '0px';
+});
+
+close.addEventListener("click", () => {
+    overlay.style.display = 'none';
+    menu.style.right = '-300px';
+});
+
+function ajustarMenu() {
+    if (window.innerWidth > 1000) {
+        menu.style.right = '0px';
+    } else {
+        menu.style.right = '-300px'
+    }
+}
+
+ajustarMenu();
+
+window.addEventListener('resize', ajustarMenu);
