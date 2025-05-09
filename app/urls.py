cb0df4d8f5d1741de94d04fe3d48051f62ad8339
enzortorres <enzortorres2005@gmail.com>
2025-05-08 21:44:09 -0300

    criando namespace para o app, e redirecionamento de páginas

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
-    path('', views.index),
-    path('adotar/', views.adotar),
-    path('apadrinhamento/', views.apadrinhamento),
+    path('', views.index, name='home'),
+    path('adotar/', views.adotar, name='adotar'),
+    path('apadrinhamento/', views.apadrinhamento, name='apadrinhamento'),
     path('admin/', admin.site.urls),
 ]
