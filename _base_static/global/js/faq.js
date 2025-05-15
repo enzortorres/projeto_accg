22e1faba671b13bd65fd8b39b21b6976afebe5dd
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-05-15 12:28:21 -0300

    finalizei o home

---
+++ _base_static/global/js/faq.js
@@ -0,0 +1,9 @@
+const faqs = document.querySelectorAll(".faq")
+
+faqs.forEach((faq) => {
+    const question = faq.querySelector(".question")
+    
+    question.addEventListener("click", () => {
+        faq.classList.toggle("active")
+    })
+})
