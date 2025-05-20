a6de75f51974e385a083964b26f8390a4c192224
enzortorres <enzortorres2005@gmail.com>
2025-05-20 00:10:44 -0300

    sidebar finalizado

--- _base_static/admin/js/close-header.js
+++ _base_static/admin/js/close-header.js
@@ -1,6 +1,23 @@
 const header = document.querySelector('#header');
-const closeHeader = document.querySelector('button.close-header');
+const closeHeader = document.querySelector('.close-header');
 
 closeHeader.addEventListener("click", () => {
-    header.classList.toggle('show');
+    header.classList.toggle('header-hidden');
 });
+
+let headerHiddenApplied = false;
+
+function handleResize() {
+    if (window.innerWidth < 1000 && !headerHiddenApplied) {
+        header.classList.add('header-hidden');
+        headerHiddenApplied = true;
+    } else if (window.innerWidth >= 1000) {
+        headerHiddenApplied = false;
+    }
+}
+
+// Chama na carga da página
+handleResize();
+
+// Monitora redimensionamento
+window.addEventListener('resize', handleResize);
